0e7ba03643b7a764cb15af055dd4b2760b6f0cf0
thijnmens <thijnsmulders04@gmail.com>
2024-07-09 17:03:57 +0200

    Add prettier

--- 1. Two Sum/main.ts
+++ 1. Two Sum/main.ts
@@ -1,19 +1,17 @@
 function twoSum(nums: number[], target: number): number[] {
-    
-    let map = new Map<number, string>();
-    
-    for (let i in nums) {
-        const needed = target - nums[i];
-        
-        if (map.has(needed)) {
-            return [Number(i), Number(map.get(needed) ?? -1)]
-        }
-        
-        
-        map.set(nums[i], i)
-    }
-    
-    return [0,0]
+	let map = new Map<number, string>();
+
+	for (let i in nums) {
+		const needed = target - nums[i];
+
+		if (map.has(needed)) {
+			return [Number(i), Number(map.get(needed) ?? -1)];
+		}
+
+		map.set(nums[i], i);
+	}
+
+	return [0, 0];
 }
 
-console.log(twoSum([2,1,9,4,4,56,90,3], 8));
+console.log(twoSum([2, 1, 9, 4, 4, 56, 90, 3], 8));
